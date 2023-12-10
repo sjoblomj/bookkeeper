2f73c2f6114cbd69a45483ea4a2d8e1a6c771624
Johan Sjöblom <sjoblomj88@gmail.com>
2023-12-10 22:39:49 +0100

    Adding first draft of project

---
+++ nordea_csv_to_json.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cat account_plan.csv | tail -n +2 | sed -E 's/\r//g; s/^.*"([0-9]+)";"([^"]*)";"([^"]*)";.*/\1;\2;\3/g' | jq -Rsn '
+[inputs
+  | . / "\n"
+  | (.[] | select(length > 0) | . / ";") as $input
+  | {"account_number": ($input[0] | tonumber? // $input[0]), "name": $input[1], "account_type": $input[2]}
+]' > account_plan.json
+
+
+cat "$1" | tail -n +2 | sed '1!G;h;$!d' | awk 'BEGIN{Num=0}{print Num++ ";" $0}' | jq -Rsn '
+def cap:
+  [splits("[, \t]") | select(length > 0)]
+  | map((.[:1]|ascii_upcase) + (.[1:]|ascii_downcase | gsub("É"; "é") | gsub("Å"; "å") | gsub("Ä"; "ä") | gsub("Ö"; "ö")))
+  | join(" ");
+
+[inputs
+  | . / "\n"
+  | (.[] | select(length > 0) | . / ";") as $input
+  | ($input[2] | gsub(","; ".") | tonumber? // $input[2]) as $amount
+  | {"id": $input[0] | tonumber, "date": $input[1] | gsub("/"; "-"), "amount": $amount, "sender": $input[3], "receiver": $input[4], "name": $input[5] | cap, "more_details": $input[6] | cap, "message": $input[7] | cap, "notes": $input[8], "account_balance": ($input[9] | sub(","; ".") | tonumber? // null), "currency": $input[10], "type": $input[12], "category": (if $amount < 0 then "negative" else "positive" end)}
+]' > plusgiro.json
+
+jq -s 'flatten | group_by(.id) | map(reduce .[] as $x ({}; . * $x))' plusgiro.json accounting.json > bookkeeping.json
